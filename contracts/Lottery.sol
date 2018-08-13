7e6895bfa60c3684041e8f01fb4a0d8db4cfd6e9
Thomas Phillips <thomasphillips3@gmail.com>
2018-08-12 17:08:42 -0700

    Initial commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+    
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        
+        players=new address[](0);
+    }
+    
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+    }
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+}
