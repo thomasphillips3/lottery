df1acd854df5fe7226b693870f5414b9e077f525
Thomas Phillips <thomasphillips3@gmail.com>
2018-08-15 23:49:07 -0700

    pickWinner test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -67,7 +67,27 @@ describe('Lottery Contract', () => {
 
     it('requires user to be the manager to pick a winner', async () => {
         try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[1]
+            });
+            assert(false);
         } catch (err) {
+            assert(err);
         }
+    });
+
+    it('pays the winner and clears the players', async() => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('1', 'ether')
+        });
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+        assert(difference > web3.utils.toWei('0.8', 'ether'));
+
+        const players = await lottery.methods.getPlayers().call({ from: accounts[0] });
+        assert.equal(0, players.length);
     });
 });
